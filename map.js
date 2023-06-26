33c051cf45dab136c038d723d11f3fa7b519fc64
Your Name <henry.mickael1@gmail.com>
2023-06-26 14:47:38 +0200

    Elite

---
+++ map.js
@@ -0,0 +1,23 @@
+function loadScript(url, callback) {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
+  script.onload = callback;
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+function initMap() {
+  const position = { lat: 49.415353406207494, lng: 6.136965485116145 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: position,
+    zoom: 17,
+  });
+  const marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    title: "L'Elite Cut & Color by A.S",
+  });
+}
+loadScript(
+  `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`
+);
